6e628a40321e88013ac57088e3ace7dabcb8f898
Nathaniel Bauernfeind <nate.bauernfeind@gmail.com>
2014-10-18 23:31:02 -0400

    Split into many submodules; prove out plugin architecture.

---
+++ khronos-app/src/main/scala/com/nefariouszhen/khronos/KhronosApplication.scala
@@ -0,0 +1,19 @@
+package com.nefariouszhen.khronos
+
+import com.nefariouszhen.khronos.db.ram.InMemoryTSDBConfiguration
+import com.nefariouszhen.khronos.engine.{KhronosApplicationBase, KhronosConfiguration}
+import com.nefariouszhen.khronos.ui.UiModule
+import com.nefariouszhen.khronos.util.DropwizardModule
+
+object KhronosApplication extends KhronosApplicationBase[KhronosConfiguration] {
+  def createModules(configuration: KhronosConfiguration): Seq[DropwizardModule[_]] = {
+    if (configuration.db == null) {
+      configuration.db = new InMemoryTSDBConfiguration
+    }
+
+    Seq(
+      configuration.db.buildModule(),
+      new UiModule
+    )
+  }
+}
--- khronos-engine/src/main/scala/com/nefariouszhen/khronos/engine/KhronosApplication.scala
+++ khronos-engine/src/main/scala/com/nefariouszhen/khronos/engine/KhronosApplication.scala
@@ -8,9 +8,6 @@ import com.google.inject.Guice
 import com.massrelevance.dropwizard.ScalaApplication
 import com.massrelevance.dropwizard.bundles.ScalaBundle
 import com.nefariouszhen.khronos.db.DatabaseConfiguration
-import com.nefariouszhen.khronos.db.cassandra.CassandraDatabaseModule
-import com.nefariouszhen.khronos.db.ram.InMemoryTSDBConfiguration
-import com.nefariouszhen.khronos.ui.UiModule
 import com.nefariouszhen.khronos.util.DropwizardModule
 import io.dropwizard.Configuration
 import io.dropwizard.setup.{Bootstrap, Environment}
@@ -21,7 +18,7 @@ class KhronosConfiguration extends Configuration with AssetsBundleConfiguration 
   override def getAssetsConfiguration = assets
 
   @JsonProperty
-  var db: DatabaseConfiguration = new InMemoryTSDBConfiguration
+  var db: DatabaseConfiguration = null
 }
 
 abstract class KhronosApplicationBase[T <: KhronosConfiguration] extends ScalaApplication[T] {
@@ -29,12 +26,14 @@ abstract class KhronosApplicationBase[T <: KhronosConfiguration] extends ScalaAp
 
   def initialize(bootstrap: Bootstrap[T]): Unit = {
     bootstrap.addBundle(new ScalaBundle)
-    bootstrap.addBundle(new ConfiguredAssetsBundle("/assets/", "/dashboard/"))
+    bootstrap.addBundle(new ConfiguredAssetsBundle("/assets/", "/khronos/"))
 
     bootstrap.addBundle(new RedirectBundle(
-      new UriRedirect("/", "/dashboard/"),
-      new UriRedirect("/index.htm", "/dashboard/"),
-      new UriRedirect("/index.html", "/dashboard/")
+      new UriRedirect("/", "/khronos/"),
+      new UriRedirect("/index.htm", "/khronos/"),
+      new UriRedirect("/index.html", "/khronos/"),
+      new UriRedirect("/khronos/index.htm", "/khronos/"),
+      new UriRedirect("/khronos/index.html", "/khronos/")
     ))
 
     bootstrap.addBundle(new WebJarBundle())
@@ -45,11 +44,4 @@ abstract class KhronosApplicationBase[T <: KhronosConfiguration] extends ScalaAp
     Guice.createInjector(modules: _*)
     modules.foreach(_.install(environment))
   }
-}
-
-object KhronosApplication extends KhronosApplicationBase[KhronosConfiguration] {
-  def createModules(configuration: KhronosConfiguration): Seq[DropwizardModule[_]] = Seq(
-    configuration.db.buildModule(),
-    new UiModule
-  )
 }
